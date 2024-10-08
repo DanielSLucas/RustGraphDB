cec6b7d77f85b5d1ba4eb49ccd14a42cf92bdc3d
Vitor Freire <vitorvfreire@outlook.com>
2024-10-08 20:18:37 -0300

    tirando necessidade de escrever --cli

--- Cli/src/main.rs
+++ Cli/src/main.rs
@@ -1,15 +1,8 @@
 use rustCli::lib::api::cli::run_cli;
-use std::env;
-use std::sync::{Arc, Mutex};
 //use ctrlc;
 
 fn main() -> std::io::Result<()> {
-    // Pass the storage_manager to API modules as needed
-    let args: Vec<String> = env::args().collect();
-
-    if args.contains(&"--cli".to_string()) {
-        run_cli();
-    }
+    run_cli();
 
     Ok(())
 }
