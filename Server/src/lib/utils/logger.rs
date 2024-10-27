d5fc6a6e26b0e66d7b118da2f1509eab3fdfbd4b
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-10-27 17:23:40 -0300

    conditional loggin

--- Server/src/lib/utils/logger.rs
+++ Server/src/lib/utils/logger.rs
@@ -1,5 +1,10 @@
+use std::env;
+
 pub fn log_info(message: &str) {
-  println!("{}", message);
+  let log = env::var("LOG").is_ok();
+  if log {
+    println!("{}", message);
+  }
 }
 
 pub fn log_error(message: &str) {
